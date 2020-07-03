cbe0e1537921cfc36f93a7c1a0ec11f9541fa6e6
Augusto <augusto_conceicao@hotmail.com>
2020-07-02 23:40:36 -0300

    Ajuste usando arquivos.

--- src/AlgoritmoAES.java
+++ src/AlgoritmoAES.java
@@ -1,3 +1,10 @@
+import javax.swing.*;
+import javax.swing.filechooser.FileSystemView;
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.atomic.AtomicInteger;
+
 public class AlgoritmoAES {
 
     final static int[] sbox = {0x63, 0x7C, 0x77, 0x7B, 0xF2, 0x6B, 0x6F, 0xC5, 0x30, 0x01, 0x67, 0x2B, 0xFE, 0xD7, 0xAB, 0x76, 0xCA, 0x82, 0xC9, 0x7D, 0xFA, 0x59, 0x47, 0xF0, 0xAD, 0xD4, 0xA2, 0xAF, 0x9C, 0xA4, 0x72, 0xC0, 0xB7, 0xFD, 0x93, 0x26, 0x36, 0x3F, 0xF7, 0xCC, 0x34, 0xA5, 0xE5, 0xF1, 0x71, 0xD8, 0x31, 0x15, 0x04, 0xC7, 0x23, 0xC3, 0x18, 0x96, 0x05, 0x9A, 0x07, 0x12, 0x80, 0xE2, 0xEB, 0x27, 0xB2, 0x75, 0x09, 0x83, 0x2C, 0x1A, 0x1B, 0x6E, 0x5A, 0xA0, 0x52, 0x3B, 0xD6, 0xB3, 0x29, 0xE3, 0x2F, 0x84, 0x53, 0xD1, 0x00, 0xED, 0x20, 0xFC, 0xB1, 0x5B, 0x6A, 0xCB, 0xBE, 0x39, 0x4A, 0x4C, 0x58, 0xCF, 0xD0, 0xEF, 0xAA, 0xFB, 0x43, 0x4D, 0x33, 0x85, 0x45, 0xF9, 0x02, 0x7F, 0x50, 0x3C, 0x9F, 0xA8, 0x51, 0xA3, 0x40, 0x8F, 0x92, 0x9D, 0x38, 0xF5, 0xBC, 0xB6, 0xDA, 0x21, 0x10, 0xFF, 0xF3, 0xD2, 0xCD, 0x0C, 0x13, 0xEC, 0x5F, 0x97, 0x44, 0x17, 0xC4, 0xA7, 0x7E, 0x3D, 0x64, 0x5D, 0x19, 0x73, 0x60, 0x81, 0x4F, 0xDC, 0x22, 0x2A, 0x90, 0x88, 0x46, 0xEE, 0xB8, 0x14, 0xDE, 0x5E, 0x0B, 0xDB, 0xE0, 0x32, 0x3A, 0x0A, 0x49, 0x06, 0x24, 0x5C, 0xC2, 0xD3, 0xAC, 0x62, 0x91, 0x95, 0xE4, 0x79, 0xE7, 0xC8, 0x37, 0x6D, 0x8D, 0xD5, 0x4E, 0xA9, 0x6C, 0x56, 0xF4, 0xEA, 0x65, 0x7A, 0xAE, 0x08, 0xBA, 0x78, 0x25, 0x2E, 0x1C, 0xA6, 0xB4, 0xC6, 0xE8, 0xDD, 0x74, 0x1F, 0x4B, 0xBD, 0x8B, 0x8A, 0x70, 0x3E, 0xB5, 0x66, 0x48, 0x03, 0xF6, 0x0E, 0x61, 0x35, 0x57, 0xB9, 0x86, 0xC1, 0x1D, 0x9E, 0xE1, 0xF8, 0x98, 0x11, 0x69, 0xD9, 0x8E, 0x94, 0x9B, 0x1E, 0x87, 0xE9, 0xCE, 0x55, 0x28, 0xDF, 0x8C, 0xA1, 0x89, 0x0D, 0xBF, 0xE6, 0x42, 0x68, 0x41, 0x99, 0x2D, 0x0F, 0xB0, 0x54, 0xBB, 0x16};
@@ -207,151 +214,200 @@ public class AlgoritmoAES {
         return state;
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
 
 
-        byte[] chave = {0x41, 0x42, 0x43, 0x44, 0x45, 0x46, 0x47, 0x48, 0x49, 0x4a, 0x4b, 0x4c, 0x4d, 0x4e, 0x4f, 0x50};
+        byte[] chave = new byte[16];
 
-        byte[] textoSimples = {0x44, 0x4e, 0x56, 0x4e, 0x45, 0x56, 0x49, 0x54, 0x53, 0x4f, 0x4d, 0x4f, 0x45, 0x4c, 0x45, 0x21};
+        JFileChooser jfc = new JFileChooser(FileSystemView.getFileSystemView().getHomeDirectory());
+        int returnValue = jfc.showOpenDialog(null);
+        File selectedFile;
 
-        keySchedule = new byte[11][4][4];
-        matrizEstado = new byte[4][4];
+        if (returnValue == JFileChooser.APPROVE_OPTION) {
+            selectedFile = jfc.getSelectedFile();
+            FileReader fr = new FileReader(selectedFile);
 
-        int coluna = 0, linha = 0;
-        for (int i = 0; i < chave.length; i++) {
-            if (i % 4 == 0 && i > 0) {
-                coluna++;
-                linha = 0;
-            }
-            keySchedule[0][coluna][linha] = chave[i];
-            linha++;
-        }
-
-        System.out.println("Chave");
-        System.out.println(bytesToHex(chave));
-
-        System.out.println("RoundKey=0");
-        System.out.println(roundKeyToHex(keySchedule[0]));
-
-        byte[][] roundkey = new byte[4][];
-
-        for (int i = 1; i <= 10; i++) {
-
-            System.out.print("RoundKey=");
-            System.out.println(i);
-
-            byte[] parteFinal = keySchedule[i-1][3];
-
-            System.out.println(bytesToHex(parteFinal));
-
-            byte[] rot = rotWord(parteFinal);
-
-            System.out.println(bytesToHex(rot));
-
-            byte[] sub = subWord(rot);
-
-            System.out.println(bytesToHex(sub));
-
-            byte[] roundConstant = {0x00, 0x00, 0x00, 0x00};
-            roundConstant[0] = getConstant(i-1);
-
-            System.out.println(bytesToHex(roundConstant));
-
-            byte[] xor = xorWord(sub, roundConstant);
-
-            System.out.println(bytesToHex(xor));
-
-            byte[] parteInicial = keySchedule[i-1][0];
-
-            System.out.println(bytesToHex(parteInicial));
-
-            byte[] xor2 = xorWord(parteInicial, xor);
-
-            System.out.println(bytesToHex(xor2));
-
-            roundkey[0] = xor2;
-
-
-            for (int j = 1; j < 4; j++) {
-                byte[] xor3 = xorWord(keySchedule[i-1][j], roundkey[j-1]);
-                roundkey[j] = xor3;
+            int i = fr.read();
+            List<Byte> byteList = new ArrayList();
+            while (i != -1) {
+                Integer integer = i;
+                byte byteValue = integer.byteValue();
+                byteList.add(byteValue);
+                i = fr.read();
             }
 
-            keySchedule[i] = roundkey.clone();
-            System.out.println(roundKeyToHex(keySchedule[i]));
+            final byte[] textoSimples = new byte[byteList.size()];
+            AtomicInteger count = new AtomicInteger();
+            count.set(0);
+            byteList.forEach(aByte -> {
+                textoSimples[count.get()] = aByte.byteValue();
+                count.getAndIncrement();
+            });
 
-        }
+            String password = JOptionPane.showInputDialog("Informe uma chave com 16 bytes separados por virgula");
+            String[] pedacos = password.split(",");
 
-        System.out.println("-----------------");
-
-        roundkey = keySchedule[0];
-
-        System.out.println("Texto Simples");
-        System.out.println(bytesToHex(textoSimples));
-
-        byte[] xorConteudo = xorWord(textoSimples, roundKeyToByte(roundkey, true));
-
-        System.out.println("AddRoundKey-Round=0");
-        System.out.println(bytesToHex(xorConteudo));
-
-        //----------
-
-        byte[] xorRound = null;
-        byte[] subConteudo = null;
-        for (int i = 1; i <= 9; i++) {
-
-            if (xorRound == null) {
-                subConteudo = subWord(xorConteudo);
-            } else {
-                subConteudo = subWord(xorRound);
+            try {
+                for (int j = 0; j < pedacos.length; j++) {
+                    chave[j] = (byte) Integer.parseInt(pedacos[j]);
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
             }
 
-            System.out.println("SubBytes-Round=" + i);
+
+            keySchedule = new byte[11][4][4];
+            matrizEstado = new byte[4][4];
+
+            int coluna = 0, linha = 0;
+            for (int k = 0; k < chave.length; k++) {
+                if (k % 4 == 0 && k > 0) {
+                    coluna++;
+                    linha = 0;
+                }
+                keySchedule[0][coluna][linha] = chave[k];
+                linha++;
+            }
+
+            System.out.println("Chave");
+            System.out.println(bytesToHex(chave));
+
+            System.out.println("RoundKey=0");
+            System.out.println(roundKeyToHex(keySchedule[0]));
+
+            byte[][] roundkey = new byte[4][];
+
+            for (int l = 1; l <= 10; l++) {
+
+                System.out.print("RoundKey=");
+                System.out.println(l);
+
+                byte[] parteFinal = keySchedule[l - 1][3];
+
+                System.out.println(bytesToHex(parteFinal));
+
+                byte[] rot = rotWord(parteFinal);
+
+                System.out.println(bytesToHex(rot));
+
+                byte[] sub = subWord(rot);
+
+                System.out.println(bytesToHex(sub));
+
+                byte[] roundConstant = {0x00, 0x00, 0x00, 0x00};
+                roundConstant[0] = getConstant(l - 1);
+
+                System.out.println(bytesToHex(roundConstant));
+
+                byte[] xor = xorWord(sub, roundConstant);
+
+                System.out.println(bytesToHex(xor));
+
+                byte[] parteInicial = keySchedule[l - 1][0];
+
+                System.out.println(bytesToHex(parteInicial));
+
+                byte[] xor2 = xorWord(parteInicial, xor);
+
+                System.out.println(bytesToHex(xor2));
+
+                roundkey[0] = xor2;
+
+
+                for (int j = 1; j < 4; j++) {
+                    byte[] xor3 = xorWord(keySchedule[l - 1][j], roundkey[j - 1]);
+                    roundkey[j] = xor3;
+                }
+
+                keySchedule[l] = roundkey.clone();
+                System.out.println(roundKeyToHex(keySchedule[l]));
+
+            }
+
+            System.out.println("-----------------");
+
+            roundkey = keySchedule[0];
+
+            System.out.println("Texto Simples");
+            System.out.println(bytesToHex(textoSimples));
+
+            byte[] xorConteudo = xorWord(textoSimples, roundKeyToByte(roundkey, true));
+
+            System.out.println("AddRoundKey-Round=0");
+            System.out.println(bytesToHex(xorConteudo));
+
+            //----------
+
+            byte[] xorRound = null;
+            byte[] subConteudo = null;
+            for (int m = 1; m <= 9; m++) {
+
+                if (xorRound == null) {
+                    subConteudo = subWord(xorConteudo);
+                } else {
+                    subConteudo = subWord(xorRound);
+                }
+
+                System.out.println("SubBytes-Round=" + m);
+                System.out.println(bytesToHex(subConteudo));
+
+                matrizEstado = shiftRows(subConteudo);
+
+                System.out.println("ShiftRows-Round=" + m);
+                System.out.println(bytesToHex(roundKeyToByte(matrizEstado, false)));
+
+                for (int j = 0; j < matrizEstado.length; j++) {
+                    matrizEstado = mixColumn(matrizEstado, j);
+                }
+
+                System.out.println("MixedColumns-Round=" + m);
+                System.out.println(bytesToHex(roundKeyToByte(matrizEstado, false)));
+
+                roundkey = keySchedule[m];
+                System.out.println("roundkey=" + m);
+                System.out.println(roundKeyToHex(roundkey));
+
+                xorRound = xorWord(roundKeyToByte(matrizEstado, false), roundKeyToByte(roundkey, true));
+
+                System.out.println("addRoundKey-Round=" + m);
+                System.out.println(bytesToHex(xorRound));
+
+            }
+
+
+            subConteudo = subWord(xorRound);
+
+            System.out.println("SubBytes-Round=10");
             System.out.println(bytesToHex(subConteudo));
 
             matrizEstado = shiftRows(subConteudo);
 
-            System.out.println("ShiftRows-Round=" + i);
+            System.out.println("ShiftRows-Round=10");
             System.out.println(bytesToHex(roundKeyToByte(matrizEstado, false)));
 
-            for (int j = 0; j < matrizEstado.length; j++) {
-                matrizEstado = mixColumn(matrizEstado, j);
-            }
-
-            System.out.println("MixedColumns-Round=" + i);
-            System.out.println(bytesToHex(roundKeyToByte(matrizEstado, false)));
-
-            roundkey = keySchedule[i];
-            System.out.println("roundkey=" + i);
-            System.out.println(roundKeyToHex(roundkey));
+            roundkey = keySchedule[10];
 
             xorRound = xorWord(roundKeyToByte(matrizEstado, false), roundKeyToByte(roundkey, true));
 
-            System.out.println("addRoundKey-Round=" + i);
+            System.out.println("addRoundKey-Round=10");
             System.out.println(bytesToHex(xorRound));
 
+            //----------
+
+
+            JFileChooser chooser = new JFileChooser();
+            chooser.setCurrentDirectory(new File("."));
+            chooser.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
+            chooser.setAcceptAllFileFilterUsed(false);
+            chooser.showOpenDialog(null);
+
+            String mm = JOptionPane.showInputDialog("Dê um nome seguido da extensão do arquivo de saída (arquivo.extensao)");
+
+            OutputStream os = new FileOutputStream(chooser.getSelectedFile() + "\\"+ mm);
+            os.write(xorRound);
+            os.close();
+
         }
-
-
-        subConteudo = subWord(xorRound);
-
-        System.out.println("SubBytes-Round=10");
-        System.out.println(bytesToHex(subConteudo));
-
-        matrizEstado = shiftRows(subConteudo);
-
-        System.out.println("ShiftRows-Round=10");
-        System.out.println(bytesToHex(roundKeyToByte(matrizEstado, false)));
-
-        roundkey = keySchedule[10];
-
-        xorRound = xorWord(roundKeyToByte(matrizEstado, false), roundKeyToByte(roundkey, true));
-
-        System.out.println("addRoundKey-Round=10");
-        System.out.println(bytesToHex(xorRound));
-
-        //----------
-
     }
 
 }
